704dac4b110f98535086ca8fa19ee51a58523ea4
James Hudson <james.hudson@pod-point.com>
2020-03-06 15:33:04 +0000

    CR - formatting

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import * as ex from 'excalibur';
-import { LevelOne } from './scenes/level-one/level-one';
-import { Resources } from './resources';
+import {LevelOne} from './scenes/level-one/level-one';
+import {Resources} from './resources';
 
 class Game extends ex.Engine {
-  constructor() {
-    super({ width: 800, height: 600, displayMode: ex.DisplayMode.FullScreen });
-  }
+    constructor() {
+        super({width: 800, height: 600, displayMode: ex.DisplayMode.FullScreen});
+    }
 
-  public start(loader: ex.Loader) {
-    return super.start(loader);
-  }
+    public start(loader: ex.Loader) {
+        return super.start(loader);
+    }
 }
 
 const game = new Game();
@@ -19,9 +19,9 @@ game.add('levelOne', levelOne);
 
 let loader = new ex.Loader();
 for (let key in Resources) {
-  loader.addResource(Resources[key]);
+    loader.addResource(Resources[key]);
 }
 
 game.start(loader).then(() => {
-  game.goToScene('levelOne');
+    game.goToScene('levelOne');
 });
